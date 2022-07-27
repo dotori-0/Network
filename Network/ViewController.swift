b39834d4443a7e13149e1243621bf287c9d25926
dotori-0 <sesan131@gmail.com>
2022-07-27 11:42:21 +0900

    Initial Commit

---
+++ Network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Network
+//
+//  Created by SC on 2022/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
